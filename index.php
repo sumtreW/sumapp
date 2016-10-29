5694940d64b68e147e2502954d076dec4d28f839
sumtreW <sumtre@163.com>
2016-10-29 18:21:01 +0800

    45

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-	$db=sqlite_open("sumtre.db");
-	$result=sqlite_query($db, "create table test (id int PRIMARY KEY auto_increment,content text,time datetime)");
+//	$db=sqlite_open("sumtre.db");
+//	$result=sqlite_query($db, "create table test (id int PRIMARY KEY auto_increment,content text,time datetime)");
+echo 123;
 ?>
 
 
